930b2b4d567c328e3176458623a5849303885276
Corvin <corvinszimion@googlemail.com>
2023-10-31 20:28:46 +0100

    Add project files.

---
+++ WPFSplashScreenDemo/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFSplashScreen;
+
+namespace WPFSplashScreenDemo
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			var settings = new SplashScreenSettings(TimeSpan.FromSeconds(5), new CustomSplashScreenA(), new MainWindow());
+			this.UseSplashScreen(settings);
+		}
+	}
+}
---
+++ WPFSplashScreenDemo/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSplashScreenDemo;
+
+internal class MainWindowViewModel
+{
+	public bool IsVisible => true;
+}
